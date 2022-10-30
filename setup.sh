0c1ebd0150f4424eb5f4b8ee2d1b3edcca492009
mdh <mdh@DESKTOP-8O89H6C.localdomain>
2022-10-30 10:43:04 +0000

    updated setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,26 @@
 #! usr/bin/bash
 
-echo "test"
+echo "Updating System"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get full-upgrade -y 
+sudo apt-get dist-upgrade -y 
+sudo apt-get autoremove -y 
+sudo apt-get autoclean -y
+
+echo "Adding Directories"
+mkdir documents
+mkdir .config
+
+echo "Installing Starship"
+curl -sS https://starship.rs/install.sh | sh
+eval "$(starship init bash)"
+touch .config/starship.toml
+starship preset nerd-font-symbols > ~/.config/starship.toml
+
+echo "Installing Nvim"
+sudo apt install fuse
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
