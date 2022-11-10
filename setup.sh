adb489bfb7568ad61178401256628efbde6ab5e0
nduvalhall <86284690+nduvalhall@users.noreply.github.com>
2022-11-10 09:26:22 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #! usr/bin/bash
 
-echo "Updating System"
+# fix resolv conf
+sudo rm /etc/resolv.conf
+sudo bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
+sudo bash -c 'echo "[network]" > /etc/wsl.conf'
+sudo bash -c 'echo "generateResolvConf = false" >> /etc/wsl.conf'
+sudo chattr +i /etc/resolv.conf
+
+# add directories
+mkdir .config
+mkdir documents
+
+# updating system
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get full-upgrade -y 
@@ -8,20 +19,25 @@ sudo apt-get dist-upgrade -y
 sudo apt-get autoremove -y 
 sudo apt-get autoclean -y
 
-echo "Installing Starship"
+# install starship
 curl -sS https://starship.rs/install.sh | sh
 echo 'eval "$(starship init bash)"' >> .bashrc
 touch .config/starship.toml
 starship preset nerd-font-symbols > ~/.config/starship.toml
+source .bashrc
 
-echo "Installing Nvim"
-sudo apt install fuse
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-echo 'alias nv="nvim"' >> .bashrc
-mv nvim.appimage ./NeoVim
+# install nvim
+git clone https://github.com/neovim/neovim.git
+cd neovim
+make
+make install
 
-sudo apt install fzf
+# install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+source .bashrc
+nvm install 19
+
+# install package managers
+sudo apt install python3-pip
+sudo apt install cargo
+source .bashrc
